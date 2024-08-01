cff66077afd7cb51bc3c535150f66e2a903ff03f
MeongW <125392666+MeongW@users.noreply.github.com>
2024-08-01 15:35:41 +0900

    Update WebConfig.java

--- src/main/java/com/example/fileupload/config/WebConfig.java
+++ src/main/java/com/example/fileupload/config/WebConfig.java
@@ -3,13 +3,17 @@ package com.example.fileupload.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${file.base-dir}")
+    private String baseDir;
+    
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/D:/fileUpload/");
+                .addResourceLocations("file:/" + baseDir);
     }
 }
