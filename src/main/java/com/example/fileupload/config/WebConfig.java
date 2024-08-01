75a82ac2a223e00f674d27acf827a566568b1638
MeongW <125392666+MeongW@users.noreply.github.com>
2024-08-01 15:57:01 +0900

    Update WebConfig.java

--- src/main/java/com/example/fileupload/config/WebConfig.java
+++ src/main/java/com/example/fileupload/config/WebConfig.java
@@ -14,6 +14,6 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/" + baseDir + "/");
+                .addResourceLocations("file:" + baseDir + "/");
     }
 }
