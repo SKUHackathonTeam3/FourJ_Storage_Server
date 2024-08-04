50aa171227b7111f44612bcad12a47cb8f8d49ac
MeongW <125392666+MeongW@users.noreply.github.com>
2024-08-04 17:34:05 +0900

    Update SecurityConfig.java

--- src/main/java/com/example/fileupload/config/SecurityConfig.java
+++ src/main/java/com/example/fileupload/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
